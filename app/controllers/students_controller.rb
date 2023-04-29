1677eea78818b17a7089aca96e65c0213f1bca2a
Hannan <najma.mahat@student.moringaschool.com>
2023-04-29 17:12:55 +0300

    Completed adding query string parameter

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
-
   def index
-    students = Student.all
-    render json: students
+    if !params[:name]
+      students = Student.all
+      render json: students
+    else
+      name = params[:name].capitalize()
+      students =
+        Student.all.select do |student|
+          student.first_name == name || student.last_name == name
+        end
+      render json: students
+    end
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
